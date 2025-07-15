e95ba808113f2742c9eddbf0b3c1a6744d9a23ce
Shariq Firdous <shariq.f.bakal@gmail.com>
2025-07-15 14:18:15 -0600

    Added redux auth

---
+++ client/src/features/auth/authSlice.js
@@ -0,0 +1,60 @@
+import {createSlice,createAsyncThunk} from "@reduxjs/toolkit"
+import authService from "./authService"
+import { act } from "react"
+
+//Get user from local storage
+
+const user = JSON.parse(localStorage.getItem("user")) //JSON.parse() takes that string and turns it into a JavaScript object. 
+
+
+
+const initialState = {
+    user : user?user:"null",
+    isError:false,
+    isSuccess :false,
+    isLoading :false,
+    message :""
+}
+//Register user
+
+export const register = createAsyncThunk("/auth/register", async(user,ThunkAPI)=>{
+    try{
+        return await authService.register()
+    }
+    catch(error){
+        const message = error.message
+        return ThunkAPI.rejectWithValue(message)
+    }
+})
+
+export const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        reset:()=>{
+            state.isError = false
+            state.isLoading = false
+            state.isSuccess = false
+            state.message = ""
+        }
+    },
+    extraReducers : (builder)=>{
+        builder.
+        addCase(register.pending,(state)=>{
+            state.isLoading = true
+        })
+        .addCase(register.fulfilled, (state,action)=>{
+            state.isLoading = false
+            state.isSuccess = true
+            state.user = action.payload
+        })
+        .addCase(register.rejected, (state, action)=>{
+            state.isLoading = false
+            state.isError = true
+            state.user = null
+            state.message = action.payload
+        })
+    }
+})
+export const {reset} = authSlice.actions
+export default authSlice.reducer
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom";
-// import { store } from './app/store.js';
-// import { Provider } from 'react-redux'
+import { store } from './app/store.js';
+import { Provider } from 'react-redux'
 
 
 const root = document.getElementById('root');
@@ -11,9 +11,9 @@ const root = document.getElementById('root');
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
-    {/* <Provider> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
